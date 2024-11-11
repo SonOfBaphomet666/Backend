93fad45d2602a6c14a55e76602ee2faa060eab5b
Maxim <burning666@list.ru>
2024-11-11 11:36:26 +0700

    Add data base

--- server.js
+++ server.js
@@ -6,23 +6,14 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(MONGO_URI);
+const dataBase = "db";
+const articlesDb = "articles";
+const detailArticle = "detailArticles";
+// const { log } = require('console');
 
-
-// const { MongoClient } = require("mongodb");
-
-// const client = new MongoClient(MONGO_URI);
-
-// const db = client.db("articles");
-// const coll = db.collection("detailArticle");
-
-// const docs = [
-//   { name: "Hui" },
-//   { name: "Govno" }
-// ];
-
-// const result = coll.insertMany(docs);
-
-// console.log(result.insertedIds);
 
 
 app.use(cors({
@@ -30,8 +21,10 @@ app.use(cors({
 }));
 
 async function start() {
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connecting to:", process.env.MONGO_URI);
+    await mongoose.connect(MONGO_URI);
     app.listen(PORT, () => {
       console.log(`Server is running on port: ${PORT}`);
     })
@@ -42,37 +35,7 @@ async function start() {
 start();
 
 
-
-
-
-const articlesPath = path.join(__dirname, 'articles.json');
-let articlesData = JSON.parse(fs.readFileSync(articlesPath, 'utf8'));
-
-
-app.get('/api/articles', (req, res) => {
-  // if (req.query.forceError === 'true') { // добавляем условие для имитации ошибки = /api/articles?forceError=true
-  //     res.status(500).json({ message: 'Internal Server Error (simulated)' });
-  //     return;
-  // }
-  if (articlesData && Array.isArray(articlesData)) {
-    let limit = parseInt(req.query.limit) || 10;
-    let offset = parseInt(req.query.offset) || 0;
-    let tag = req.query.tag;
-    let articles = [];
-
-    if (tag) {
-      articles = articlesData.filter((article) => article.tag === tag).slice(offset, offset + limit);
-    } else {
-      articles = articlesData.slice(offset, offset + limit);
-    }
-    // res.status(500).json({ message: 'Internal Server Error' });
-    // setTimeout()
-    res.json(articles);
-  } else {
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
-
+// DETAIL ARTICLES. DON'T USE THE DATA BASE.
 app.get('/api/article/:id', (req, res) => {
   const articleId = req.params.id;
   const filePath = path.join(__dirname, 'article', `${articleId}.json`);
@@ -84,59 +47,75 @@ app.get('/api/article/:id', (req, res) => {
     res.status(404).json({ message: "Article not found" });
   }
 });
+// 
+
+// const articlesPath = path.join(__dirname, 'articles.json');
+// let articlesData = JSON.parse(fs.readFileSync(articlesPath, 'utf8'));
+
+// ARTICLES. USES A DATA BASE.
+app.get('/api/articles', async (req, res) => {
+  try {
+    const db = client.db(dataBase);
+    const article = db.collection(articlesDb);
+    const cursor = article.find();
+    res.json(cursor);
+  } catch {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+
 
 app.post('/api/login', (res) => {
   res.send('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c')
 })
 
-
-// app.post('/api/article', async (req, res) => {
-//   const data = req.body;
-//   const path = './article.json';
-
-//   try {
-//     let articles = await fs.promises.readFile(path, 'utf8');
-//     if (!articles) {
-//       articles = [];
-//     } else {
-//       articles = JSON.parse(articles);
-//     }
-
-//     const newArticleId = Math.max(...articles.map(article => article.id), 0) + 1;
-//     articles.push({ id: newArticleId, ...data });
-//     await fs.promises.writeFile(path, JSON.stringify(articles, null, 2));
-//     res.send(`Article created with ID ${newArticleId}`);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Error creating article');
-//   }
-// });
-
 app.use(express.json());
 
+// METHOD POST USES A DATA BASE.
 app.post('/api/article', async (req, res) => {
   const data = req.body;
-  const folderPath = './news/';
-  let nextId = 1;
-
+  const db = client.db(dataBase);
+  const article = db.collection(articlesDb);
   try {
-    // read existing articles to find the next available ID
-    const files = await fs.promises.readdir(folderPath);
-    const articleIds = files.map(file => parseInt(file.replace('.json', '')));
-    if (articleIds.length > 0) {
-      nextId = Math.max(...articleIds) + 1;
-    }
+    const articleData = { ...data };
+    await article.insertOne(articleData);
+    const result = db.article.find();
+    console.log(result);
 
-    const filePath = `${folderPath}${nextId}.json`;
-    const articleData = { id: nextId, ...data };
-
-    await fs.promises.writeFile(filePath, JSON.stringify(articleData, null, 2));
-    res.send(`Article created with ID ${nextId}`);
+    // console.log(article);
+    res.send('Article created successfully');
   } catch (error) {
     console.error(error);
     res.status(500).send('Error creating article');
   }
 });
+// 
+
+
+// app.post('/api/article', async (req, res) => {
+//   const data = req.body;
+//   const folderPath = './news/';
+//   let nextId = 1;
+
+//   try {
+//     // read existing articles to find the next available ID
+//     const files = await fs.promises.readdir(folderPath);
+//     const articleIds = files.map(file => parseInt(file.replace('.json', '')));
+//     if (articleIds.length > 0) {
+//       nextId = Math.max(...articleIds) + 1;
+//     }
+
+//     const filePath = `${folderPath}${nextId}.json`;
+//     const articleData = { id: nextId, ...data };
+
+//     await fs.promises.writeFile(filePath, JSON.stringify(articleData, null, 2));
+//     res.send(`Article created with ID ${nextId}`);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Error creating article');
+//   }
+// });
 
 
 
